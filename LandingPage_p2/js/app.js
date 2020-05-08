673a2b3d068ef99468da94cc9d2e446f5ff5d132
Ujjwal Singh <44271512+ujjwalsingh017@users.noreply.github.com>
2020-05-08 20:04:44 +0530

    Add files via upload

---
+++ LandingPage_p2/js/app.js
@@ -0,0 +1,58 @@
+const navbar = document.querySelector('#navbar__list');
+const sections = document.querySelectorAll('section')
+function getActiveElem() {
+    maxSection = sections[0];
+    minVal = 1000000;
+    for (item of sections) {
+        let bounding = item.getBoundingClientRect();
+        if (bounding.top > -300 & bounding.top < minVal) {
+            minVal = bounding.top;
+            maxSection = item;
+        };
+    };
+    return maxSection;
+};
+function addSections() {
+    for (let item of sections) {
+        let section = document.createElement('li');
+        section.className = 'menu__link';
+        section.dataset.nav = item.id;
+        section.innerText = item.dataset.nav;
+        navbar.appendChild(section);
+    };
+};
+function setActive () {
+    window.addEventListener('scroll', function (event) {
+        let section = getActiveElem();
+        section.classList.add('your-active-class');
+        // set other sections as inactive
+        for (let item of sections) {
+            if (item.id != section.id & item.classList.contains('your-active-class')) {
+                item.classList.remove('your-active-class');
+            }
+        }
+        // set corresponding header style
+        const active = document.querySelector('li[data-nav="' + section.id + '"]');
+        active.classList.add('active__link');
+        // remove from other headers
+        const headers = document.querySelectorAll('.menu__link');
+        for (let item of headers) {
+            console.log(item);
+            if (item.dataset.nav != active.dataset.nav & item.classList.contains('active__link')) {
+                item.classList.remove('active__link');
+            }
+        };
+    });
+};
+function scrollToClick() {
+    navbar.addEventListener('click', function (event) {
+        const clicked = document.querySelector('#' + event.target.dataset.nav)
+        clicked.scrollIntoView();
+    });
+};
+
+
+addSections();
+scrollToClick();
+setActive();
+
